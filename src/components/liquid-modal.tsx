6f91517eac5569de4127e7560c4075a829463023
ChainMasterTR - Mustafa SARI <34295148+musxos@users.noreply.github.com>
2023-07-28 11:41:27 +0300

    gg

--- src/components/liquid-modal.tsx
+++ src/components/liquid-modal.tsx
@@ -82,10 +82,12 @@ export function useLiquid({ pool }: { pool: Pool }) {
       <h1 className="px-6 text-xl font-medium mb-6">Add Liquidity</h1>
       <div className="text-sm mx-5 rounded-lg mt-2 mb-6 px-6 py-4 text-violet-500 bg-[#E8DEFD]">
         <b>Yield Farming: </b>
-        
-        Yield Farming allows you to earn passive income by investing in DeFi protocols. 
-        However, liquidity providers may experience impermanent loss due to price fluctuations of tokens. 
-        It's important to invest carefully, considering this risk.
+        <p>
+          Yield Farming allows you to earn passive income by investing in DeFi protocols. 
+          However, liquidity providers may experience impermanent loss due to price fluctuations of tokens. 
+          Its important to invest carefully, considering this risk.
+        </p>
+ 
       </div>
       <button
         onClick={() => setModalOpen(false)}

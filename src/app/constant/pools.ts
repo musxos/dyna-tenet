a2bc94f3d6a9731bac38868e7362bfbc6d79bd2a
ChainMasterTR - Mustafa SARI <34295148+musxos@users.noreply.github.com>
2024-12-21 20:58:53 +0300

    Update pools.ts

--- src/app/constant/pools.ts
+++ src/app/constant/pools.ts
@@ -36,7 +36,7 @@ export const Pools: Pool[] = [
     target: {
       address: "0x2994ea5e2DEeE06A6181f268C3692866C4BE6E9b",
       price: 0.2,
-      symbol: "wTENET",
+      symbol: "wABS",
       image: "/wtenet.png",
     },
     pool: {
